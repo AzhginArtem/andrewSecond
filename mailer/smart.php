2c6e3263fcbba1f5af5d69be78a9d61e7d10bd37
XsteP <forroyalee2@gmail.com>
2023-08-09 23:35:17 +0300

    add files

---
+++ mailer/smart.php
@@ -0,0 +1,37 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'forroyalee2@gmail.com';                 // Наш логин
+$mail->Password = 'yuzsgkvquppgwycl';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('', 'Web-site');
+$mail->addAddress('azhginartemdeveloper@gmail.com'); 
+$mail->isHTML(true);            
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+	header('Location: ../index.html');
+    return true;
+}
+
+?>
